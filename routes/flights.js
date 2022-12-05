9029e85f77b572a4d08425a509be47a66279b8f3
seanevanlee <seanevanlee@gmail.com>
2022-12-05 01:19:48 -0800

    progress2, need some guidance for tickets

--- routes/flights.js
+++ routes/flights.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const flightCtrl = require("../controllers/flights");
-// const { countDocuments } = require("../models/flight");
 
 /* GET users listing. */
 router.get("/new", flightCtrl.new);
