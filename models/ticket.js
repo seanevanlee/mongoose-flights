3d69f296fd47673e7965b533cf6f12101c0d1dc4
seanevanlee <seanevanlee@gmail.com>
2022-12-05 01:21:56 -0800

    fixed typo, need ticket help

--- models/ticket.js
+++ models/ticket.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ticketSchema = new Schema({
-  sseat: {
+  seat: {
     type: String,
     match: /[A-F][1-9]\d?/,
   },
